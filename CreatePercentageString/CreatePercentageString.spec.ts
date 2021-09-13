ee6a9663183c089a80333f165c9d268f7a2af061
Daniel Garcia <daniel.garcia@b2wdigital.com>
2021-09-13 13:08:29 -0300

    test(jest): configuring jest

--- CreatePercentageString/CreatePercentageString.spec.ts
+++ CreatePercentageString/CreatePercentageString.spec.ts
@@ -10,4 +10,9 @@ describe('#ToolBox - CreatePercentageString', () => {
     const percentString = createPercentageString(130, 22);
     expect(percentString).toEqual('16,92%');
   });
+
+  it('should return 0% for total equals to 0', () => {
+    const percentString = createPercentageString(0, 22);
+    expect(percentString).toEqual('0%');
+  });
 });
